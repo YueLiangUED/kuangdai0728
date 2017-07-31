dce9d6b9775dad09b9879fa92a0760405ae62c88
ChengYien <cye_orange@163.com>
2017-07-31 10:07:18 +0800

    a

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2017-05-26 11:03:33
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-05-26 11:03:58
+* @Last Modified time: 2017-07-31 10:02:40
 */
 
 $(document).ready(function(){
@@ -11,5 +11,20 @@ $(document).ready(function(){
 
     $('.form-edit-select select').on('tap', function () {
     	$(this).css('color', '#333');
+    });
+    
+
+    // 链接跳转
+    $('.step-btn.enter-step').on('click', function () {
+        window.location.href = 'step_04.html';
+    });
+    $('.step-btn.enter-step-return').on('click', function () {
+        history.go(-1);
+    });
+    $('.step-btn.enter-video').on('click', function () {
+        window.location.href = 'videoQ_04.html';
+    });
+    $('.step-btn.enter-step-return').on('click', function () {
+        history.go(-1);
     });
 });
